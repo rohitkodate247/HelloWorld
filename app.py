bab1a1fa14bd16dc3d334cc7aa79e0899c0f9de1
Rohit Kodate <53255939+rohitkodate247@users.noreply.github.com>
2025-09-17 18:37:46 -0600

    CICD

---
+++ app.py
@@ -0,0 +1,11 @@
+# dictionaries (hash maps)
+
+def word_count(text):
+    counts = {}
+    for word in text.split():
+        counts[word] = counts.get(word, 0) + 1
+
+    return counts
+
+
+print(word_count("Fitch Fitch Rating"))
---
+++ dictionaries(hash maps).py
@@ -0,0 +1,12 @@
+# dictionaries (hash maps)
+
+def word_count(text):
+    counts = {}
+    for word in text.split():
+        counts[word] = counts.get(word, 0) + 1
+
+    return counts
+
+
+print(word_count("Fitch Fitch Rating"))
+x
