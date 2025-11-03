4822f91030f28c7114372818890b3e1c894e7a35
Rohit Kodate <53255939+rohitkodate247@users.noreply.github.com>
2025-11-03 00:16:50 -0700

    Added few more questions

--- Exceptionhandling.py
+++ Exceptionhandling.py
@@ -1,3 +1,4 @@
+# Using Multiple except Blocks
 try:
     with open("file.txt", "r") as file:
         data = file.read()
@@ -8,3 +9,22 @@ except ValueError:
     print("File content is not a valid number.")
 except ZeroDivisionError:
     print("Division by zero error!")
+
+# Using else Block
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+else:
+    print("The result is:", result)
+
+# Using finally Block
+try:
+    file = open("file.txt", "r")
+    data = file.read()
+except FileNotFoundError:
+    print("File not found.")
+finally:
+    print("Closing the file.")
+    # file.close()  # Ensures resources are released
